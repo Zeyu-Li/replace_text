25a909c482888c5b7896dda3326aa2e3dfb20766
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-05-10 14:45:59 -0600

    reorangized things

--- replace.py
+++ replace.py
@@ -13,10 +13,7 @@ This program replaces words in a string object with words in a dictionary
 import json
 
 
-def main():
-    """ Takes string and replaces it with values in dictionary """
-
-    string = """Once upon a time a Wolf was lapping at a spring on a hillside,
+string = """Once upon a time a Wolf was lapping at a spring on a hillside,
 when, looking up, what should he see but a Lamb just
 beginning to drink a little lower down. ‘There’s my supper,’
 thought he, ‘if only I can find some excuse to seize it.’ Then
@@ -30,36 +27,40 @@ names this time last year?’
 ‘That cannot be,’ said the Lamb; ‘I am only six months
 old.’"""
 
+
+def replace(dictionary):
+
+    index = 0
+    # for words in the text, split by line break or space
+    for word in string.replace("\n", " ").split(" "):
+
+        # if it is in the dictionary, change the word
+        # to the one in the dictionary
+        if word.lower() in dictionary:
+            if word not in dictionary and word != "I":
+                word = dictionary[word.lower()].title()
+            else:
+                word = dictionary[word.lower()]
+
+        print(word, end=' ')
+        index += 1
+
+        # new line break every 9 words
+        if index % 10 == 9:
+            print("")
+
+
+def main():
+    """ Takes string and replaces it with values in dictionary """
+
     # opens json dictionary
+    assert "fail"
     try:
         with open('replace.json', 'r') as fp:
             dictionary = json.load(fp)
-
-        index = 0
-        # for words in the text, split by line break or space
-        for word in string.replace("\n", " ").split(" "):
-
-            # if it is in the dictionary, change the word
-            # to the one in the dictionary
-            if word.lower() in dictionary:
-                if word not in dictionary and word != "I":
-                    word = dictionary[word.lower()].title()
-                else:
-                    word = dictionary[word.lower()]
-
-            print(word, end=' ')
-            index += 1
-
-            # new line break every 9 words
-            if index % 10 == 9:
-                print("")
-
-    except:
-        fail = True
-
-    if not fail:
+        replace(dictionary)
         print("The translation was successful")
-    else:
+    except:
         print("Failed to translation because json was not given")
 
     return 0
